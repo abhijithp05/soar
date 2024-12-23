783d9c0d93757cc30573ec1eb1a768a710007b76
admin <admin@admins-MacBook-Pro.local>
2024-12-23 18:19:31 +0530

    soar: fix side bar and top nav component

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as SoarIcon } from 'assets/icons/soar.svg';
-import { sideBarConstants } from 'constants/sideBar';
+import { useAppContext } from 'context/AppContext';
+import { sideBarConstants } from 'constants/appConstants';
 import { SideNavContainer } from '../styles/StyledContainers';
 import { StyledLink } from '../styles/StyledLink';
 import Icon from '../ui/Icon';
-import { Link } from 'react-router-dom';
 
-export const SideBar = () => {
+const SideBar = () => {
+  const [selected, setSelected] = useState('Dashboard');
+  const { setApplicationContext } = useAppContext();
+
+  const handleLinkClick = useCallback(
+    (value) => {
+      setApplicationContext({ pageName: value.title });
+      setSelected(value.label);
+    },
+    [setApplicationContext]
+  );
+
   return (
     <SideNavContainer>
       <nav>
         <div className="flex flex-row items-center mb-10">
           <Icon icon={SoarIcon} className="w-9 h-9" />
-          <StyledLink className="font-extrabold text-2xl leading-8">
+          <StyledLink className="font-inter font-extrabold text-2xl leading-8 text-black-100">
             Soar Task
           </StyledLink>
         </div>
         <ul className="space-y-4">
-          {Object.entries(sideBarConstants)?.map(([key, value]) => (
+          {Object.entries(sideBarConstants).map(([key, value]) => (
             <li key={key} className="flex flex-row items-center ml-2">
-              {value.icon}
+              <Icon
+                icon={value.icon}
+                className="w-6 h-6"
+                color={selected === value.label ? '#000' : '#B1B1B1'}
+              />
               <Link
-                className="font-medium leading-5 text-lg block px-4 py-2 text-black hover:text-gray-700 rounded"
+                className={`font-medium leading-5 text-lg block px-4 py-2 ${value.disabled ? 'pointer-events-none' : ''} ${selected === value.label ? 'text-black' : 'text-gray-400'} hover:text-gray-700 rounded`}
+                onClick={() => handleLinkClick(value)}
                 to={value.path}
               >
                 {value.label}
@@ -33,3 +50,5 @@ export const SideBar = () => {
     </SideNavContainer>
   );
 };
+
+export default React.memo(SideBar);
--- src/components/layout/TopNav.jsx
+++ src/components/layout/TopNav.jsx
@@ -5,12 +5,26 @@ import { ReactComponent as SettingIcon } from 'assets/icons/setting-nav.svg';
 import { ReactComponent as AvatarIcon } from 'assets/icons/avatar.svg';
 import { Input, Button } from '../ui';
 import { TopNavContainer } from '../styles/StyledContainers';
+import { useAppContext } from 'context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
-export const TopNav = () => {
+const TopNav = () => {
+  const navigate = useNavigate();
+  const {
+    appContext: { pageName },
+    setApplicationContext,
+  } = useAppContext();
+
+  const handleSettingIconClick = () => {
+    setApplicationContext({ pageName: 'Setting' });
+    navigate('/settings');
+  };
   return (
     <TopNavContainer>
       <div className="flex justify-between items-center">
-        <div className="flex items-center text-black">Dashboard</div>
+        <div className="flex items-center text-black-100 text-3xl font-semibold leading-9 font-inter">
+          {pageName}
+        </div>
         <div className="flex items-center space-x-4">
           <div className="flex items-center  border-gray-300 bg-light-gray rounded-full p-2 max-w-xs w-full">
             <SearchIcon className="mr-2 ml-2" />
@@ -20,7 +34,7 @@ export const TopNav = () => {
             />
           </div>
 
-          <Button className="p-2 rounded">
+          <Button className="p-2 rounded" onClick={handleSettingIconClick}>
             <SettingIcon />
           </Button>
           <Button className="p-2 rounded">
@@ -34,3 +48,5 @@ export const TopNav = () => {
     </TopNavContainer>
   );
 };
+
+export default React.memo(TopNav);
